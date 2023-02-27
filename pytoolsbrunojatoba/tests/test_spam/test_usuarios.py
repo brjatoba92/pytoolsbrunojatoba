c63deafc2526e8bf3e8d535af101293696d0c473
brjatoba92 <brjatoba92@gmail.com>
2023-02-27 16:46:27 -0300

    Setup and Tear Down with fixture

--- pytoolsbrunojatoba/tests/test_spam/test_usuarios.py
+++ pytoolsbrunojatoba/tests/test_spam/test_usuarios.py
@@ -1,23 +1,30 @@
 from pytoolsbrunojatoba.spam.db import Conexao
 from pytoolsbrunojatoba.spam.modelos import Usuario
+import pytest
 
-def test_salvar_usuario():
-    conexao = Conexao()
-    sessao = conexao.gerar_sessao()
+@pytest.fixture
+def conexao():
+    #Setup
+    conexao_obj = Conexao()
+    yield conexao_obj
+    #Tear Down
+    conexao_obj.fechar()
+
+@pytest.fixture
+def sessao(conexao):
+    sessao_obj = conexao.gerar_sessao()
+    yield sessao_obj
+    sessao_obj.roll_back()
+    sessao_obj.fechar()
+
+def test_salvar_usuario(sessao):
     usuario = Usuario(nome='Bruno')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
-    sessao.roll_back()
-    sessao.fechar()
-    conexao.fechar()
+    
 
-def test_listar_usuarios():
-    conexao = Conexao()
-    sessao = conexao.gerar_sessao()
+def test_listar_usuarios(sessao):
     usuarios = [Usuario(nome='Bruno'), Usuario(nome='Isabella')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
-    sessao.roll_back()
-    sessao.fechar()
-    conexao.fechar()
