d77db8f09e80ae5754c23ce03fccf4aa4a8cf5d6
brjatoba92 <brjatoba92@gmail.com>
2023-02-28 19:03:54 -0300

    Mock Implemetion

--- pytoolsbrunojatoba/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ pytoolsbrunojatoba/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -2,6 +2,7 @@ from pytoolsbrunojatoba.spam.enviador_de_email import Enviador
 from pytoolsbrunojatoba.spam.main import EnviadorDeSpam
 from pytoolsbrunojatoba.spam.modelos import Usuario
 import pytest
+from unittest.mock import Mock
 
 @pytest.mark.parametrize(
         'usuarios',
@@ -19,36 +20,27 @@ import pytest
 def test_qtd_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'brjatoba92@djangosolutions.com',
         'DevPro Course',
         'Check the amazings modules'
     )
-    assert len(usuarios) == enviador.qtd_email_enviados
+    assert len(usuarios) == enviador.enviar.call_count
 
-class EnviadorMock(Enviador):
-    def __init__(self):
-        super().__init__()
-        self.qtd_email_enviados = 0
-        self.parametros_de_envio = None
-
-    def enviar(self, remetente, destinatario, assunto, corpo):
-        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
-        self.qtd_email_enviados += 1
 
 def test_parametros_de_spam(sessao):
     usuario = Usuario(nome='Bruno', email='brjatoba92@djangosolutions.com')
     sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'bellaragao@google.com',
         'DevPro Course',
         'Check the amazings modules'
     )
-    assert enviador.parametros_de_envio == (
+    enviador.enviar.assert_called_once_with == (
         'bellaragao@google.com',
         'brjatoba92@djangosolutions.com',
         'DevPro Course',
