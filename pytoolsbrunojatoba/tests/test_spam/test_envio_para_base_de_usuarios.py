381e7861fd892a43f18288af3905335ce9dd2a78
brjatoba92 <brjatoba92@gmail.com>
2023-02-28 17:28:36 -0300

    Dependence injection

--- pytoolsbrunojatoba/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ pytoolsbrunojatoba/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -15,14 +15,42 @@ import pytest
             ]
         ]
 )
+
 def test_qtd_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = Enviador()
+    enviador = EnviadorMock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
-        'brjatoba@djangosolutions.com',
+        'brjatoba92@djangosolutions.com',
         'DevPro Course',
         'Check the amazings modules'
     )
     assert len(usuarios) == enviador.qtd_email_enviados
+
+class EnviadorMock(Enviador):
+    def __init__(self):
+        super().__init__()
+        self.qtd_email_enviados = 0
+        self.parametros_de_envio = None
+
+    def enviar(self, remetente, destinatario, assunto, corpo):
+        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
+        self.qtd_email_enviados += 1
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Bruno', email='brjatoba92@djangosolutions.com')
+    sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'bellaragao@google.com',
+        'DevPro Course',
+        'Check the amazings modules'
+    )
+    assert enviador.parametros_de_envio == (
+        'bellaragao@google.com',
+        'brjatoba92@djangosolutions.com',
+        'DevPro Course',
+        'Check the amazings modules'
+    )
