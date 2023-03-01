e747ce7a7f8c9b8442326738026a06eb772d22e2
brjatoba92 <brjatoba92@gmail.com>
2023-03-01 15:17:25 -0300

    Imports isolation

--- pytoolsbrunojatoba/tests/test_spam/test_github_api.py
+++ pytoolsbrunojatoba/tests/test_spam/test_github_api.py
@@ -1,12 +1,25 @@
 from unittest.mock import Mock
 from pytoolsbrunojatoba import github_api
+import pytest
 
-def test_buscar_avatar():
+def test_buscar_avatar(avatar_url):
+    url = github_api.buscar_avatar('brjatoba92')
+    assert avatar_url == url
+
+@pytest.fixture
+def avatar_url():
     resp_mock = Mock()
+    url = 'https://avatars.githubusercontent.com/u/69490377?v=4'
     resp_mock.json.return_value = {
         'login': 'brjatoba92', 'id': 69490377,
-        'avatar_url': 'https://avatars.githubusercontent.com/u/69490377?v=4',
+        'avatar_url': url,
     }
+    get_original = github_api.rq.get
     github_api.rq.get = Mock(return_value=resp_mock)
+    yield url
+    github_api.rq.get = get_original
+    
+
+def test_buscar_avatar_integracao():
     url = github_api.buscar_avatar('brjatoba92')
     assert 'https://avatars.githubusercontent.com/u/69490377?v=4' == url
